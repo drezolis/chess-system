5c4b1f078714b1b05db9dfb52e37b49cf54d5f14
drezolis <dregoncalves22@gmail.com>
2024-03-06 20:44:48 -0300

    BoardException and defensive programming

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,17 +4,19 @@ import chess.ChessPiece;
 
 public class UI {
 
+    // É estático para não precisar instanciar a classe UI.
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
-            System.out.println();
+            System.out.println();   
         }
         System.out.println("  a b c d e f g h");
     }
 
+    // Lógica de impressão de uma peça.
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,38 +6,63 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int colums) {
+        if (rows < 1 || colums < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column.");
+        }
         this.rows = rows;
         this.colums = colums;
         pieces = new Piece[rows][colums];
     }
+
     public int getRows() {
         return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
     }
 
     public int getColums() {
         return colums;
     }
 
-    public void setColums(int colums) {
-        this.colums = colums;
-    }
-
-    // Método que retorna a posição da peça na matriz.
-    public Piece piece(int row, int column){
+    // Método que retorna a posição da peça na matriz. Há um tratamento de exceção
+    // que verifica se aquela posição existe.
+    public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on the board.");
+        }
         return pieces[row][column];
     }
 
-    //sobrecarga do método piece
-    public Piece piece(Position position){
+    // Sobrecarga do método piece.
+    public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
-    public void placePiece(Piece piece, Position position){
+    // Atribui à posição correspondente a peça (piece) passada como argumento.
+    // Verifica se naquela posição já existe uma peça e, se sim, lança uma exceção.
+    public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position: " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    // Verifica se a peça está dentro do tabuleiro.
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < colums;
+    }
+
+    // Reaproveitamento.
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board.");
+        }
+        return piece(position) != null;
     }
 }
