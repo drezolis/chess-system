90bf82e3f43b6a339f0c1a15fb8e638a9cb4aa05
Andre Gonçalves <dregoncalves22@gmail.com>
2024-03-11 11:45:37 -0300

    Possible moves of a piece

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
     private Color color;
 
     // No construtor da classe que herdou, deve conter todos os atributos da classe herdada. Caso eu não tivesse feito esse construtor na classe que herdou (ChessPiece), haveria um erro de compilação por que a superclasse(Piece) não tem um construtor padrão. O programa não conseguiria instanciar a classe ChessPiece justamente por não ter um construtor acessível.
